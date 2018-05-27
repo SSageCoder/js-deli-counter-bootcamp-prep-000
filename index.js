ccfa019e26e88042b7c947c60520295708cb4161
ssingle2002@gmail.com <ssingle2002@gmail.com>
2018-05-27 18:12:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,3 @@
+var katzDeli = [];
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
